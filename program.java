1b2d1740c4e41f148d6bc72bbe31b69bfeff1848
Dharshini G <145423878+dharshinig22@users.noreply.github.com>
2024-04-27 12:16:31 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+class program
+{
+public static void main(string[] args)
+{
+scanner value=new scanner(system.in);
+int a=value.nextint();
+int b=value.nextint();
+int c=a+b;
+system.out.println(c);
+}
+}
---
+++ table.java
@@ -0,0 +1,14 @@
+public class table
+public static void main(String[] args)
+{
+int n=5;
+int i=1;
+system.out.println("enter the value of multiplication number");
+}
+while(i<=10)
+{
+system.out.println(n+"*"+1+"=i(n=1))
+i++;
+}
+
+
