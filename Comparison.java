b78b72ef86d86186fd9295f353b6767f057da44a
Dharshini G <145423878+dharshinig22@users.noreply.github.com>
2024-07-03 11:40:26 +0530

    Practice question
    Write a program to perform number comparison using various relational operators like >, >=, <, <=, == and !=. For example, consider two numbers 15 and 20. 15 < 20 is true. 15 <= 20 is true. 15 > 20 is false. 15 >= 20 is false. 15 == 20 is false. 15 != 20 is true.

---
+++ Comparison.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Comparison{
+
+public static void main(String[] args){
+
+Scanner scan=new Scanner(System.in);
+
+System.out.println("enter the first number");
+int num1=scan.nextInt();
+
+System.out.println("enter the second number");     
+int num2=scan.nextInt();
+ 
+System.out.println(num1 + " < " + num2 + " is " + (num1 < num2));
+       
+ System.out.println(num1 + " <= " + num2 + " is " + (num1 <= num2));
+       
+ System.out.println(num1 + " > " + num2 + " is " + (num1 > num2));
+        
+System.out.println(num1 + " >= " + num2 + " is " + (num1 >= num2));
+        
+System.out.println(num1 + " == " + num2 + " is " + (num1 == num2));
+       
+ System.out.println(num1 + " != " + num2 + " is " + (num1 != num2));
+   
+ }
+
+}
+
+
